315a1ac0293678d28298655ab053f1c49d486d06
Patrick <torre@rotwang.com>
2022-08-20 22:56:07 -0700

    Make builds a little more robust and repeatable.

--- buildah-build.sh
+++ buildah-build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
-ls -l /dev/fuse
 
-buildah build --timestamp=$(date '+%s') \
-    --logfile buildah-build.log -t public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8 .
+buildah build --logfile buildah-build.log \
+    -t public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8 .
 
 if [ $? -eq 0 ]; then
-    buildah login -u AWS --password-stdin < ecr-login-password public.ecr.aws/v6u6a9m5 &&
-    buildah push public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8
+    (buildah login -u AWS --password-stdin < ecr-login-password public.ecr.aws/v6u6a9m5 &&
+     buildah push public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8) &>> buildah-build.log
 fi
