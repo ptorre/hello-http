d51d7ee3ec135b67bd164f598db7e100dbe7c107
Patrick <torre@rotwang.com>
2022-10-09 16:43:41 -0700

    Use init container and emptyDir to get src.

--- buildah-build.sh
+++ buildah-build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+# Build container and push to public ecr registry.  This script is
+# intended to be used by the k8s job defined in buildah-job.yaml
 
 buildah build -t public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8 .
 
@@ -6,4 +8,3 @@ if [ $? -eq 0 ]; then
     (buildah login -u AWS --password $ECR_PASSWORD public.ecr.aws/v6u6a9m5 &&
      buildah push public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8)
 fi
-
