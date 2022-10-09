0e2744723ba71d502b590349f525e83894ad605c
Patrick <torre@rotwang.com>
2022-10-09 16:04:48 -0700

    Use default stdout and stderr logging

--- buildah-build.sh
+++ buildah-build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
-buildah build --logfile buildah-build.log \
-    -t public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8 .
+buildah build -t public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8 .
 
 if [ $? -eq 0 ]; then
     (buildah login -u AWS --password $ECR_PASSWORD public.ecr.aws/v6u6a9m5 &&
-     buildah push public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8) &>> buildah-build.log
+     buildah push public.ecr.aws/v6u6a9m5/hello-http:latest-arm64v8)
 fi
 
