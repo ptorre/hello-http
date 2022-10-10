f091d34d4a9fea482b42360fe46d9858e7ad1354
Patrick <torre@rotwang.com>
2022-10-10 08:46:59 -0700

    Support the usual probes

--- hello-http.py
+++ hello-http.py
@@ -3,6 +3,11 @@
 from socket import gethostname
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http import HTTPStatus
+from time import time
+import sys
+
+unhealthy = False
+start_time = time()
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -11,13 +16,44 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        self.send_response(200)
-        msg = f"hello from {gethostname()}\n"
-        content = msg.encode(encoding="utf-8", errors="replace")
-        self.send_header("Content-Type", "text/plain;charset=utf-8")
-        self.send_header("Content-Length", str(len(content)))
-        self.end_headers()
-        self.wfile.write(content)
+        global unhealthy
+        global start_time
+        if self.path == "/health" or self.path == "/live":
+            if unhealthy is True:
+                self.send_error(HTTPStatus.INTERNAL_SERVER_ERROR)
+            else:
+                self.send_response(200)
+                self.end_headers()
+        elif self.path == "/ready":
+            if time() - start_time < 10:
+                self.send_error(HTTPStatus.SERVICE_UNAVAILABLE)
+            else:
+                self.send_response(200)
+                self.end_headers()
+        elif self.path == "/get-sick":
+            unhealthy = True
+            self.send_response(200)
+            self.end_headers()
+        elif self.path == "/get-well":
+            unhealthy = False
+            self.send_response(200)
+            self.end_headers()
+        elif self.path == "/die":
+            self.log_message("Time to die")
+            self.send_response(200)
+            self.end_headers()
+            self.flush_headers()
+            sys.exit(0)
+        elif self.path == "/":
+            self.send_response(200)
+            msg = f"hello from {gethostname()}\n"
+            content = msg.encode(encoding="utf-8", errors="replace")
+            self.send_header("Content-Type", "text/plain;charset=utf-8")
+            self.send_header("Content-Length", str(len(content)))
+            self.end_headers()
+            self.wfile.write(content)
+        else:
+            self.send_error(HTTPStatus.NOT_FOUND)
 
     def do_OPTIONS(self):
         self.send_response(204)
